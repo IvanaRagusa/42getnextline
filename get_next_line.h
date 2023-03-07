4da944293f27709b01f64e698c14bc6bf6349ad2
Nelly <115358202+IvanaRagusa@users.noreply.github.com>
2023-03-07 19:28:42 +0100

    Moulinette tested 125%

---
+++ get_next_line.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iragusa <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/17 18:44:52 by iragusa           #+#    #+#             */
+/*   Updated: 2022/12/17 18:45:01 by iragusa          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 40
+# endif
+
+# include "get_next_line.h"
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
+size_t	ft_strlen(char *str);
+int		ft_findchar(char c, char *set);
+char	*ft_read(int fd, char *ret);
+char	*ft_line(char *ret);
+char	*ft_next_ret(char *ret);
+#endif
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,114 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iragusa <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/17 18:46:14 by iragusa           #+#    #+#             */
+/*   Updated: 2022/12/17 18:46:21 by iragusa          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+void	ft_bzero(void *s, size_t n)
+{
+	char	*dest;
+	size_t	i;
+
+	dest = s;
+	i = 0;
+	while (i < n)
+	{
+		dest[i] = 0;
+		i++;
+	}
+}
+
+int	ft_findchar(char c, char *s)
+{
+	int	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	if (c == '\0')
+		return (1);
+	while (s[i] != '\0')
+	{
+		if (s[i] == (char) c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+size_t	ft_strlen(char *str)
+{
+	size_t	c;
+
+	c = 0;
+	if (!str)
+		return (0);
+	while (str[c] != '\0')
+		c++;
+	return (c);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	size_t	i;
+	size_t	c;
+	char	*str;
+
+	if (!s1)
+	{
+		s1 = (char *)malloc(1 * sizeof(char));
+		s1[0] = '\0';
+	}
+	if (!s1 || !s2)
+		return (NULL);
+	str = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	i = -1;
+	c = 0;
+	if (s1)
+		while (s1[++i] != '\0')
+			str[i] = s1[i];
+	while (s2[c] != '\0')
+		str[i++] = s2[c++];
+	str[ft_strlen(s1) + ft_strlen(s2)] = '\0';
+	free(s1);
+	return (str);
+}
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	char	*ptr;
+	size_t	i;
+
+	i = size * count;
+	ptr = malloc(i);
+	if (!ptr)
+		return (NULL);
+	ft_bzero(ptr, i);
+	return (ptr);
+}
+/*int main()
+{
+    char s[] = "  lorem\n ipsum dolor sit\n amet, Suspendisse";
+    char **ptr;
+    int    i;
+    
+    i = 0;
+    ptr = ft_split2(s);
+    while (i < 3)
+    {
+        printf("%s\n", ptr[i]);
+        write(1, "\n", 1);
+        i++;
+    }
+    return (0);
+}*/
